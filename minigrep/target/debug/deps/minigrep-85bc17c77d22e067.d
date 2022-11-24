b3f53521a17bca758a4eefe826d5a6ef37aec3ba
Wameuh <wameuh@gmail.com>
2022-11-24 14:55:41 +0200

    Removing target folder

--- minigrep/target/debug/deps/minigrep-85bc17c77d22e067.d
+++
@@ -1,5 +0,0 @@
-/home/romain/Documents/myminigrep/minigrep/target/debug/deps/minigrep-85bc17c77d22e067: src/main.rs
-
-/home/romain/Documents/myminigrep/minigrep/target/debug/deps/minigrep-85bc17c77d22e067.d: src/main.rs
-
-src/main.rs:
--- minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/home/romain/Documents/myminigrep/minigrep/target/debug/minigrep: /home/romain/Documents/myminigrep/minigrep/src/main.rs
