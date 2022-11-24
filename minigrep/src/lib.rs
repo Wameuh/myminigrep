9470a627e0b9e64eeab4c8bd4e23aad65bbc10c0
Wameuh <wameuh@gmail.com>
2022-11-24 18:37:39 +0200

    Adding some test and search function

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -19,6 +19,63 @@ pub fn run (arguments: Args) -> Result<(), Box<dyn Error>> {
     println!("reading file {}", &arguments.file_path);
     let content = fs::read_to_string(arguments.file_path)?;
 
-    println!("With text:\n{content}");
+    let lines = search(&arguments.query, content.as_str());
+
+    if lines.len() == 0 {
+        println!("'{}' not found in the file", arguments.query);
+    } else {
+        println!("{}", lines.iter().fold(String::new(), |acc, &arg| acc + arg + "\n"));
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut retval: Vec<&'a str>= vec![];
+    for line in contents.lines() {
+        if line.contains(query) {
+            retval.push(line);
+        }
+    }
+    retval
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn search_no_result() {
+        let query = "dact";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!({let a:Vec<&str> = vec![];a}, search(query, contents));
+
+    }
+
+    #[test]
+    fn search_multiples_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three duct.";
+
+        assert_eq!(vec!["safe, fast, productive.","Pick three duct."], search(query, contents));
+
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,14 +5,13 @@ use minigrep::Args;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
     let arguments = Args::build(&args).unwrap_or_else(|_| {
         println!("Error in the number of arguments provided");
         process::exit(1);
     });
     
     minigrep::run(arguments).unwrap_or_else(|err| {
-        println!("Error while reading the file. Error: {}", err);
+        println!("Application error. Error: {}", err);
         process::exit(1);
     });
 }
