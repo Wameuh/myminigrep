2b9d9982f1358c631f13db0f3eebd15445566a2a
Wameuh <wameuh@gmail.com>
2022-11-25 09:39:24 +0200

    Changing error printing into eprintln

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,12 @@ use minigrep::Args;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let arguments = Args::build(&args).unwrap_or_else(|_| {
-        println!("Error in the number of arguments provided");
+        eprintln!("Error in the number of arguments provided");
         process::exit(1);
     });
     
     minigrep::run(arguments).unwrap_or_else(|err| {
-        println!("Application error. Error: {}", err);
+        eprintln!("Application error. Error: {}", err);
         process::exit(1);
     });
 }
